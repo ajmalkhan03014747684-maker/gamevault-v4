ba4ba43a1b418c411981497d7af9b39d9feb19b1
ajmalkhan03014747684-maker <ajmalkhan03014747684@gmail.com>
2026-08-20 10:49:22 +0500

    Convert CyberTextField to StatefulWidget
    Refactored CyberTextField to be a StatefulWidget for password visibility toggle functionality.

--- lib/widgets/cyber_text_field.dart
+++ lib/widgets/cyber_text_field.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 
 /// Dark glass-style text input used on Login/Register, Withdraw, etc.
-class CyberTextField extends StatelessWidget {
+///
+/// When obscureText is true (i.e. this is a password field), an eye
+/// icon appears letting the user toggle show/hide â€” self-contained,
+/// so every existing password field in the app gets this for free.
+class CyberTextField extends StatefulWidget {
   final String hint;
   final IconData icon;
   final bool obscureText;
@@ -19,6 +23,13 @@ class CyberTextField extends StatelessWidget {
   });
 
   @override
+  State<CyberTextField> createState() => _CyberTextFieldState();
+}
+
+class _CyberTextFieldState extends State<CyberTextField> {
+  late bool _obscured = widget.obscureText;
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
@@ -27,15 +38,25 @@ class CyberTextField extends StatelessWidget {
         border: Border.all(color: AppColors.glassBorder),
       ),
       child: TextField(
-        controller: controller,
-        obscureText: obscureText,
-        keyboardType: keyboardType,
+        controller: widget.controller,
+        obscureText: _obscured,
+        keyboardType: widget.keyboardType,
         style: AppText.body(size: 15, color: AppColors.text),
         cursorColor: AppColors.primaryPurple,
         decoration: InputDecoration(
-          hintText: hint,
+          hintText: widget.hint,
           hintStyle: AppText.body(size: 15, color: AppColors.muted),
-          prefixIcon: Icon(icon, color: AppColors.muted, size: 20),
+          prefixIcon: Icon(widget.icon, color: AppColors.muted, size: 20),
+          suffixIcon: widget.obscureText
+              ? IconButton(
+                  icon: Icon(
+                    _obscured ? Icons.visibility_off_rounded : Icons.visibility_rounded,
+                    color: AppColors.muted,
+                    size: 20,
+                  ),
+                  onPressed: () => setState(() => _obscured = !_obscured),
+                )
+              : null,
           border: InputBorder.none,
           contentPadding:
               const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
