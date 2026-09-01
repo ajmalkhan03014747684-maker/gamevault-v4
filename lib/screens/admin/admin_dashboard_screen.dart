6e7a3870f5399ebb984070601159b031353f83ae
ajmalkhan03014747684-maker <ajmalkhan03014747684@gmail.com>
2026-09-01 15:59:12 +0500

    Remove unused callbacks from AdminDashboardScreen
    Removed unused callbacks for Missions and Referral Configs.

--- lib/screens/admin/admin_dashboard_screen.dart
+++ lib/screens/admin/admin_dashboard_screen.dart
@@ -7,12 +7,10 @@ class AdminDashboardScreen extends StatefulWidget {
   final VoidCallback onExit;
   final VoidCallback onPayoutsTapped;
   final VoidCallback onGamesTapped;
-  final VoidCallback onMissionsTapped;
   final VoidCallback onAntiBotTapped;
   final VoidCallback onUsersTapped;
   final VoidCallback onSettingsTapped;
   final VoidCallback onDangerZoneTapped;
-  final VoidCallback onReferralConfigsTapped;
   final VoidCallback onWithdrawReqTapped;
 
   const AdminDashboardScreen({
@@ -20,12 +18,10 @@ class AdminDashboardScreen extends StatefulWidget {
     required this.onExit,
     required this.onPayoutsTapped,
     required this.onGamesTapped,
-    required this.onMissionsTapped,
     required this.onAntiBotTapped,
     required this.onUsersTapped,
     required this.onSettingsTapped,
     required this.onDangerZoneTapped,
-    required this.onReferralConfigsTapped,
     required this.onWithdrawReqTapped,
   });
 
@@ -119,8 +115,6 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
                 onTap: widget.onPayoutsTapped,
               ),
               _AdminMenuRow(icon: Icons.sports_esports_rounded, label: 'Manage Games', onTap: widget.onGamesTapped),
-              _AdminMenuRow(icon: Icons.flag_rounded, label: 'Missions', onTap: widget.onMissionsTapped),
-              _AdminMenuRow(icon: Icons.group_add_rounded, label: 'Referral Configs', onTap: widget.onReferralConfigsTapped),
               _AdminMenuRow(icon: Icons.account_balance_wallet_rounded, label: 'Withdraw Requirements', onTap: widget.onWithdrawReqTapped),
               _AdminMenuRow(icon: Icons.shield_rounded, label: 'Anti-Bot / Flagged Users', onTap: widget.onAntiBotTapped),
               _AdminMenuRow(icon: Icons.person_off_rounded, label: 'Manage Users / Ban', onTap: widget.onUsersTapped),
